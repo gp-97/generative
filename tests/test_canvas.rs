f22f6ab9bf9fee4c3dff8e856060b7346bc26548
Gaurav Patel <GauravPatel97@outlook.com>
2021-04-23 20:05:02 +0530

    Some changes to be included

--- tests/test_canvas.rs
+++ tests/test_canvas.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 use generative::canvas::Canvas;
-use photon_rs::native::{open_image, save_image};
-use photon_rs::PhotonImage;
+use photon_rs::native::save_image;
+
 #[test]
 fn test_init() {
     let canv = Canvas::new(512, 512);
@@ -25,9 +25,32 @@ fn test_get_pixel_at() {
 
 #[test]
 fn test_set_pixel_at() {
-    let mut canv = Canvas::new(512, 512);
-    for i in 10..200 {
+    let mut canv = Canvas::new(500, 500);
+
+    // Left Eye
+    for i in 70..170 {
         for j in 100..200 {
+            canv.set_pixel_at(i, j, (180_u8, 2_u8, 50_u8, 255_u8));
+        }
+    }
+
+    // Right Eye
+    for i in 70..170 {
+        for j in 300..400 {
+            canv.set_pixel_at(i, j, (180_u8, 2_u8, 50_u8, 255_u8));
+        }
+    }
+
+    // Nose
+    for i in 150..300 {
+        for j in 225..275 {
+            canv.set_pixel_at(i, j, (180_u8, 2_u8, 50_u8, 255_u8));
+        }
+    }
+
+    // Mouth
+    for i in 350..420 {
+        for j in 50..450 {
             canv.set_pixel_at(i, j, (180_u8, 2_u8, 50_u8, 255_u8));
         }
     }
