0c7708ccafea42245b499d881281619a4bbe566b
Gaurav Patel <GauravPatel97@outlook.com>
2021-04-28 19:24:02 +0530

    Some code clearing in examples/perlin_loop.rs

--- examples/perlin_loop.rs
+++ examples/perlin_loop.rs
@@ -1,13 +1,11 @@
 use generative::canvas::Canvas;
 use generative::shape::shape2d::Polygon;
-
 use generative::Transform;
 use perlin2d::PerlinNoise2D;
 use photon_rs::native::save_image;
 
 fn setup() -> Canvas {
     let mut canvas = Canvas::new(3840 * 2, 2160 * 2);
-    // canvas.fill((255, 255, 255, 255));
     canvas.fill((0, 0, 0, 255));
     canvas
 }
@@ -16,7 +14,6 @@ fn display(canvas: &mut Canvas) {
     let mut zoff = 0.1;
     let mut tx = 1_f32;
     let mut ty = 1_f32;
-
     while zoff < 10.0 {
         let noise = PerlinNoise2D::new(4, 3.0, 0.5, 0.75, 10.0, (100.0, 100.0), 0.5, 101);
         let mut points = vec![];
@@ -30,18 +27,17 @@ fn display(canvas: &mut Canvas) {
                 f32::sin(theta as f32) * r as f32 + 100.0 * ty,
             ));
             theta += 0.01;
-            // color = (255, 0, 0, 25);
         }
         let color = (((zoff * 0.0) % 255.0) as u8, 192, 200, 255);
         tx += 0.001;
         ty += 0.001;
         zoff += 0.01;
-        let mut line = Polygon::new(points, color, 1);
-        line.transform(Transform::TRANSLATE(
+        let mut poly = Polygon::new(points, color, 1);
+        poly.transform(Transform::TRANSLATE(
             canvas.get_height() as f32 / 2.0,
             canvas.get_width() as f32 / 2.0,
         ));
-        line.draw(canvas);
+        poly.draw(canvas);
     }
     save_image(Canvas::to_photon(&canvas), "examples/outputs/perlin_loop.png");
 }
