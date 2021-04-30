1b393e2ac46d51bdf2db2d1b197e30aa1edee434
Gaurav Patel <GauravPatel97@outlook.com>
2021-05-01 01:22:03 +0530

    Added some more helper functions

--- src/helpers.rs
+++ src/helpers.rs
@@ -20,3 +20,27 @@ fn factorial(n: usize) -> usize {
 pub fn comb(n: usize, r: usize) -> usize {
     factorial(n) / (factorial(n - r) * factorial(r))
 }
+
+pub fn map(val: f32, start_1: f32, end_1: f32, start_2: f32, end_2: f32, clamp: bool) -> f32 {
+    if start_1 == end_1 {
+        val
+    } else {
+        let new_val = ((val - start_1) / (end_1 - start_1)) * (end_2 - start_2) + start_2;
+        if !clamp {
+            return new_val;
+        }
+        if start_2 < end_2 {
+            f32::clamp(new_val, start_2, end_2)
+        } else {
+            f32::clamp(new_val, end_2, start_2)
+        }
+    }
+}
+
+pub fn lerp(start: f32, end: f32, amt: f32) -> f32 {
+    start * (1.0 - amt) + end * amt
+}
+
+pub fn norm(val: f32, start: f32, end: f32) -> f32 {
+    map(val, start, end, 0.0, 1.0, false)
+}
--- tests/test_helpers.rs
+++ tests/test_helpers.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
-use generative::helpers::comb;
-use generative::helpers::linspace;
+use generative::helpers::*;
 #[test]
 fn test_linspace() {
     assert_eq!(linspace(2.0, 3.0, 5), vec![2.0, 2.25, 2.5, 2.75, 3.0]);
@@ -8,4 +7,16 @@ fn test_linspace() {
 #[test]
 fn test_factorial() {
     assert_eq!(comb(5, 2), 10);
+}
+#[test]
+fn test_map() {
+    assert_eq!(map(20.0, 0.0, 100.0, 40.0, 50.0, false), 42.0);
+}
+#[test]
+fn test_lerp() {
+    assert_eq!(lerp(20.0, 100.0, 0.2), 36.0);
+}
+#[test]
+fn test_norm() {
+    assert_eq!(norm(50.0, 0.0, 100.0), 0.5);
 }
