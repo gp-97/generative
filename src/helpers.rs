a7885f1c5e9ec20f4aeebac9d99fbec01ecc0db1
Gaurav Patel <GauravPatel97@outlook.com>
2021-05-07 00:24:41 +0530

    Added sort for Points vector

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,5 @@
+use crate::Point;
+
 pub fn linspace(start: f32, end: f32, npoints: u32) -> Vec<f32> {
     let diff = (end - start) / (npoints as f32 - 1.0);
     let mut points = vec![start];
@@ -45,6 +47,47 @@ pub fn norm(val: f32, start: f32, end: f32) -> f32 {
     map(val, start, end, 0.0, 1.0, false)
 }
 
-pub fn euclid_dist(p1: (f32, f32), p2: (f32, f32)) -> f32 {
-    ((p1.0 - p2.0).powf(2.0) + (p1.1 - p2.1).powf(2.0)).powf(0.5)
+pub fn euclid_dist(p1: &Point, p2: &Point) -> f32 {
+    ((p1.get_x() - p2.get_x()).powf(2.0) + (p1.get_y() - p2.get_y()).powf(2.0)).powf(0.5)
+}
+
+fn merge_sort(arr: &mut Vec<Point>, start: usize, end: usize, temp_arr: &mut Vec<Point>) {
+    if start < end {
+        let mid = start + (end - start) / 2;
+        merge_sort(arr, start, mid, temp_arr);
+        merge_sort(arr, mid + 1, end, temp_arr);
+
+        let mut i = start;
+        let mut j = end;
+        let mut k = start;
+
+        while i <= mid {
+            temp_arr[i] = arr[i];
+            i += 1;
+        }
+        while j > mid {
+            temp_arr[i] = arr[j];
+            i += 1;
+            j -= 1;
+        }
+        i = start;
+        j = end;
+        while k <= end {
+            if temp_arr[i] < temp_arr[j] {
+                arr[k] = temp_arr[i];
+                i += 1;
+            } else {
+                arr[k] = temp_arr[j];
+                j -= 1;
+            }
+            k += 1;
+        }
+    }
+}
+
+pub fn sort(arr: &mut Vec<Point>) {
+    let mut temp_arr = arr.clone();
+    let start = 0;
+    let end = arr.len() - 1;
+    merge_sort(arr, start, end, &mut temp_arr);
 }
